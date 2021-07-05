288124110d0051101f9f0147c3e35001c876d265
matt-ashton <48946291+matt-ashton@users.noreply.github.com>
2021-07-05 19:25:04 +0100

    update data

--- exemplar/1/data.js
+++ exemplar/1/data.js
@@ -50,5 +50,39 @@ var data = {
       "caption": "Email resent",
       "imgref": "images/5.email-resent.png"
     }]
+  },
+  {
+    "title": "Tell us about you - single person, no children",
+    "path": [{
+      "caption": "Task list",
+      "imgref": "images/3.task-list.png"
+    },{
+      "caption": "Name",
+      "imgref": "images/2.Enter-your-name.png"
+    },{
+      "caption": "Date of birth",
+      "imgref": "images/3.date-of-birth.png",
+    },{
+      "caption": "Nationality",
+      "imgref": "images/4.Enter-your-nationality.png"
+    },{
+      "caption": "Bailiwick address",
+      "imgref": "images/5.Bailiwick-address.png"
+    },{
+      "caption": "Email",
+      "imgref": "images/6.Email-address.png"
+    },{
+      "caption": "Phone number",
+      "imgref": "images/7.phone-number.png"
+    },{
+      "caption": "Marital status",
+      "imgref": "images/8.Marital-status.png"
+    },{
+      "caption": "Children",
+      "imgref": "images/9.children.png"
+    },{s
+      "caption": "Task list",
+      "imgref": "images/10.task-list-tell-us-about-you-end.png"
+    }]
   }
   ]}
