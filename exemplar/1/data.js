38c5cfdad17feb0813b1485a8e6342ae206ea0a6
matt-ashton <48946291+matt-ashton@users.noreply.github.com>
2021-07-06 16:08:16 +0100

    Update data.js

--- exemplar/1/data.js
+++ exemplar/1/data.js
@@ -191,5 +191,72 @@ var data = {
       "caption": "Task list",
       "imgref": "images/11.Task-list-married-no-children.png"
     }]
+  },
+  {
+    "title": "Tell us about you - Married person, claims family allowance",
+    "path": [{
+      "caption": "Task list",
+      "imgref": "images/3.task-list.png"
+    },{
+      "caption": "Name",
+      "imgref": "images/2.Enter-your-name.png"
+    },{
+      "caption": "Date of birth",
+      "imgref": "images/3.date-of-birth.png",
+    },{
+      "caption": "Nationality",
+      "imgref": "images/4.Enter-your-nationality.png"
+    },{
+      "caption": "Bailiwick address",
+      "imgref": "images/5.Bailiwick-address.png"
+    },{
+      "caption": "Email",
+      "imgref": "images/6.Email-address.png"
+    },{
+      "caption": "Phone number",
+      "imgref": "images/7.phone-number.png"
+    },{
+      "caption": "Marital status",
+      "imgref": "images/8.Marital-status.png"
+    },{
+      "caption": "Children",
+      "imgref": "images/9.children.png"
+    },{
+      "caption": "Child's details",
+      "imgref": "images/10.child-details.png"
+    },{
+      "caption": "Country of birth",
+      "imgref": "images/11.Country-of-birth.png"
+    },{
+      "caption": "Family allowance",
+      "imgref": "images/12.claim-family-allowance.png"
+    },{
+      "caption": "Identity documents",
+      "imgref": "images/13.identity-documents.png"
+    },{
+      "caption": "Summary of children",
+      "imgref": "images/14.Child-summary.png"
+    },{
+      "caption": "Living arrangement",
+      "imgref": "images/15.Living-arrangement.png"
+    },{
+      "caption": "Partner's details",
+      "imgref": "images/16.partners-details.png"
+    },{
+      "caption": "Partner's residence",
+      "imgref": "images/17.Partners-residence.png"
+    },{
+      "caption": "Claim elsewhere",
+      "imgref": "images/18.Family-allowance-elsewhere.png"
+    },{
+      "caption": "How you want it paid",
+      "imgref": "images/19.How-you-want-paying.png"
+    },{
+      "caption": "Bank details",
+      "imgref": "images/20.Enter-banks-details.png"
+    },{
+      "caption": "Task list",
+      "imgref": "images/21.task-list-single-person-family-allowance.png"
+    }]
   }
 ]}
