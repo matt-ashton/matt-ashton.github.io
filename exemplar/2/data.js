b78af13b329c1850c858d40c4eb805546e43d8f6
matt-ashton <48946291+matt-ashton@users.noreply.github.com>
2021-08-09 20:56:09 +0100

    add badge

--- exemplar/2/data.js
+++ exemplar/2/data.js
@@ -1,5 +1,5 @@
 var data = {
-  "service": "1. Check your current coding notice",
+  "service": "1. Check your current coding notice"<span class="hmcts-badge">Recently added</span>,
   "last-updated": "09 August 2021",
   "userjourneys": [
     {
