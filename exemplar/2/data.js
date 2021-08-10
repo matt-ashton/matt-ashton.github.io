b4bff1fdf97048aa5332b0caf48e34a8f1e3c18c
matt-ashton <48946291+matt-ashton@users.noreply.github.com>
2021-08-10 08:42:41 +0100

    update scenarios

--- exemplar/2/data.js
+++ exemplar/2/data.js
@@ -19,55 +19,7 @@ var data = {
         "imgref": "images/RS-account.png"
       },{
         "caption": "Coding notice",
-        "imgref": "images/C/N-unavailable.png"
-      }]
-    },
-    {
-      "title": "Post a vacancy",
-      "path": [{
-        "caption": "Home",
-        "imgref": "images/1.home.png",
-        "note": "User clicks 'Employer'."
-      },{
-        "caption": "Employer options",
-        "imgref": "images/22.employer.png",
-        "note": "User clicks 'Post a vacancy'."
-      },{
-        "caption": "Post a vacancy",
-        "imgref": "images/10.post-vacancy.png",
-        "note": "User clicks 'Start now' button."
-      },{
-        "caption": "Sign in",
-        "imgref": "images/11.post-vacancy-sign-in.png",
-        "note": "User enters login details then clicks 'Sign in'."
-      },{
-        "caption": "Review your details",
-        "imgref": "images/12.review-details.png",
-        "note": "User checks and amends their details then clicks 'Save and continue'."
-      },{
-        "caption": "Add vacancy details",
-        "imgref": "images/13.add-vacancy-details.png",
-        "note": "User enters vacancy details then clicks 'Save and continue'."
-      },{
-        "caption": "Add training details",
-        "imgref": "images/14.training-details.png",
-        "note": "User enters training details then clicks 'Save and continue'."
-      },{
-        "caption": "Add apprentice details",
-        "imgref": "images/15.apprentice-details.png",
-        "note": "User enters apprentice details then clicks 'Save and continue'."
-      },{
-        "caption": "Add additional information",
-        "imgref": "images/16.additional-information.png",
-        "note": "User enters any additional information then clicks 'Preview'."
-      },{
-        "caption": "Check details",
-        "imgref": "images/17.check-details.png",
-        "note": "User checks their details then clicks 'Finish and submit'."
-      },{
-        "caption": "Done",
-        "imgref": "images/18.submit-role.png",
-        "note": "Done. 'Submit another role' takes you back to 'Add vacancy details' screen."
+        "imgref": "images/CN-unavailable.png"
       }]
     }
   ]}
