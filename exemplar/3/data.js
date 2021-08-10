555f822f5d5548c9eed71f6ad8015b36633d9ebd
matt-ashton <48946291+matt-ashton@users.noreply.github.com>
2021-08-10 10:26:43 +0100

    Update data.js

--- exemplar/3/data.js
+++ exemplar/3/data.js
@@ -3,7 +3,7 @@ var data = {
   "last-updated": "10 August 2021",
   "userjourneys": [
     {
-      "title": "Confirm your details",
+      "title": "Tell us about income that is incorrect",
       "path": [{
         "caption": "Start page",
         "imgref": "images/redundancy/start-page.png"
@@ -40,7 +40,7 @@ var data = {
       }]
     },
     {
-      "title": "Part 1: Job details",
+      "title": "Tell us about income that is missing",
       "path": [{
         "caption": "Intro",
         "imgref": "images/redundancy/part-1.1.intro.png"
@@ -65,7 +65,7 @@ var data = {
       }]
     },
     {
-      "title": "Part 2: Details of wage and working hours",
+      "title": "Tell us about income that has ended",
       "path": [{
         "caption": "Intro",
         "imgref": "images/redundancy/part-2.1.intro.png"
@@ -84,121 +84,6 @@ var data = {
       },{
         "caption": "Summary",
         "imgref": "images/redundancy/part-2.6.summary.png"
-      }]
-    },
-    {
-      "title": "Part 3: Details of redundancy pay",
-      "path": [{
-        "caption": "Intro",
-        "imgref": "images/redundancy/part-3.1.intro.png"
-      },{
-        "caption": "Redundancy pay",
-        "imgref": "images/redundancy/part-3.2.redundancy-pay.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/part-3.3.summary.png"
-      }]
-    },
-    {
-      "title": "Part 4: Details of breaks in employment",
-      "path": [{
-        "caption": "Intro",
-        "imgref": "images/redundancy/part-4.1.breaks.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/part-4.2.summary.png"
-      }]
-    },
-    {
-      "title": "Part 5: Details of holiday pay",
-      "path": [{
-        "caption": "Intro",
-        "imgref": "images/redundancy/part-5.1.intro.png"
-      },{
-        "caption": "Start date",
-        "imgref": "images/redundancy/part-5.2.start-date.png"
-      },{
-        "caption": "Holiday days",
-        "imgref": "images/redundancy/part-5.3.holiday-days.png"
-      },{
-        "caption": "Days taken",
-        "imgref": "images/redundancy/part-5.4.days-taken.png"
-      },{
-        "caption": "Days not paid for",
-        "imgref": "images/redundancy/part-5.5.days-not-paid-for.png"
-      },{
-        "caption": "Dates taken",
-        "imgref": "images/redundancy/part-5.6.dates-taken.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/part-5.7.summary.png"
-      }]
-    },
-    {
-      "title": "Part 6: Details of wages owed",
-      "path": [{
-        "caption": "Intro",
-        "imgref": "images/redundancy/part-6.1.intro.png"
-      },{
-        "caption": "Dates",
-        "imgref": "images/redundancy/part-6.2.dates.png"
-      },{
-        "caption": "Amount",
-        "imgref": "images/redundancy/part-6.3.amount.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/part-6.4.summary.png"
-      }]
-    },
-    {
-      "title": "Part 7: Details of money owed to employer",
-      "path": [{
-        "caption": "Intro",
-        "imgref": "images/redundancy/part-7.1.intro.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/part-7.2.summary.png"
-      }]
-    },
-    {
-      "title": "Part 8: Details of new employment",
-      "path": [{
-        "caption": "Intro",
-        "imgref": "images/redundancy/part-8.1.intro.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/part-8.2.summary.png"
-      }]
-    },
-    {
-      "title": "Part 9: Additional information about your claim",
-      "path": [{
-        "caption": "Intro",
-        "imgref": "images/redundancy/part-9.1.intro.png"
-      },{
-        "caption": "Attachment of earnings",
-        "imgref": "images/redundancy/part-9.2.attachment-of-earnings.png"
-      },{
-        "caption": "Other info",
-        "imgref": "images/redundancy/part-9.3.other-info.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/part-9.4.summary.png"
-      }]
-    },    {
-      "title": "Payment details",
-      "path": [{
-        "caption": "Summary and declaration",
-        "imgref": "images/redundancy/7.claim-summary.png"
-      },{
-        "caption": "Payment method",
-        "imgref": "images/redundancy/8.payment-method.png"
-      },{
-        "caption": "Account details",
-        "imgref": "images/redundancy/9.account-details.png"
-      },{
-        "caption": "Done",
-        "imgref": "images/redundancy/10.done.png"
       }]
     }
   ]}
