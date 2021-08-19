27388db4ef15cb6c9c53f4e2e916df29f60cd725
matt-ashton <48946291+matt-ashton@users.noreply.github.com>
2021-08-19 06:14:24 +0100

    udpate flows

--- exemplar/4/data.js
+++ exemplar/4/data.js
@@ -1,107 +1,45 @@
 var data = {
-  "service": "2. Tell us about your other income",
+  "service": "3. Tell us about your allowances and reliefs",
   "last-updated": "10 August 2021",
   "userjourneys": [
     {
-      "title": "Tell us about income that is incorrect",
+      "title": "Tell us about an allowance or relief that is incorrect",
       "path": [{
         "caption": "Start page",
-        "imgref": "images/redundancy/start-page.png"
+        "imgref": "images/allowances-&-reliefs-start-page.png"
       },{
         "caption": "Change type",
-        "imgref": "images/redundancy/tell-us-about-incorrect.png"
+        "imgref": "images/allowance-change-type-incorrect.png"
       },{
-        "caption": "Add correct income",
-        "imgref": "images/redundancy/incorrect-income-1.png"
+        "caption": "Add incorrect allowance",
+        "imgref": "images/incorrect-allowance-1.png"
       },{
-        "caption": "Income added",
-        "imgref": "images/redundancy/incorrect-income-added.png"
+        "caption": "Allowance added",
+        "imgref": "images/incorrect-allowance-added-1.png"
       },{
-        "caption": "Add correct income",
-        "imgref": "images/redundancy/add-another-incorrect-income.png"
+        "caption": "Add incorrect allowance",
+        "imgref": "images/incorrect-allowance-2.png"
       },{
-        "caption": "Income added",
-        "imgref": "images/redundancy/second-incorrect-income-added.png"
+        "caption": "Allowance added",
+        "imgref": "images/Incorrect-allowances-added-2.png"
       },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/incorrect-income-summary.png"
+        "caption": "Add incorrect allowance",
+        "imgref": "images/incorrect-allowance-3.png"
       },{
-        "caption": "Declaration",
-        "imgref": "images/redundancy/declaration.png"
-      },{
-        "caption": "Confirmation",
-        "imgref": "images/redundancy/confirmation.png"
-      },{
-        "caption": "Feedback",
-        "imgref": "images/redundancy/feedback.png"
-      }]
-    },
-    {
-      "title": "Tell us about income that is missing",
-      "path": [{
-        "caption": "Start page",
-        "imgref": "images/redundancy/start-page.png"
-      },{
-        "caption": "Change type",
-        "imgref": "images/redundancy/change-type-missing.png"
-      },{
-        "caption": "Add missing income",
-        "imgref": "images/redundancy/add-missing-income-1.png"
-      },{
-        "caption": "income added",
-        "imgref": "images/redundancy/missing-income-1-added.png"
-      },{
-        "caption": "Add missing income",
-        "imgref": "images/redundancy/other-income-missing-2.png"
-      },{
-        "caption": "Income added",
-        "imgref": "images/redundancy/missing-incomes-added-2.png"
+        "caption": "Allowance added",
+        "imgref": "images/incorrect-allowance-added-3.png"
       },{
         "caption": "Summary",
-        "imgref": "images/redundancy/missing-income-summary.png"
+        "imgref": "images/allowance-incorrect-summary.png"
       },{
         "caption": "Declaration",
-        "imgref": "images/redundancy/declaration.png"
+        "imgref": "images/allowance-declaration.png"
       },{
         "caption": "Confirmation",
-        "imgref": "images/redundancy/confirmation.png"
+        "imgref": "images/allowance-confirmation.png"
       },{
         "caption": "Feedback",
-        "imgref": "images/redundancy/feedback.png"
-      }]
-    },
-    {
-      "title": "Tell us about income that has ended",
-      "path": [{
-        "caption": "Start page",
-        "imgref": "images/redundancy/start-page.png"
-      },{
-        "caption": "Change type",
-        "imgref": "images/redundancy/change-type-income-ended.png"
-      },{
-        "caption": "Add ended income",
-        "imgref": "images/redundancy/income-ended-1.png"
-      },{
-        "caption": "income added",
-        "imgref": "images/redundancy/ended-income-added-1.png"
-      },{
-        "caption": "Add ended income",
-        "imgref": "images/redundancy/ended-income-2.png"
-      },{
-        "caption": "Income added",
-        "imgref": "images/redundancy/ended-income-added-2.png"
-      },{
-        "caption": "Summary",
-        "imgref": "images/redundancy/ended-summary.png"
-      },{
-        "caption": "Declaration",
-        "imgref": "images/redundancy/declaration.png"
-      },{
-        "caption": "Confirmation",
-        "imgref": "images/redundancy/confirmation.png"
-      },{
-        "caption": "Feedback",
-        "imgref": "images/redundancy/feedback.png"
+        "imgref": "images/allowances-feedback.png"
       }]
     }
   ]}
