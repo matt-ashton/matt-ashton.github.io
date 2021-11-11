4c4d5efba2a36c93af2e524b5d970b92b4b6b84d
matt-ashton <48946291+matt-ashton@users.noreply.github.com>
2021-11-11 00:09:58 +0000

    update pension section

--- exemplar/1/data.js
+++ exemplar/1/data.js
@@ -622,8 +622,14 @@ var data = {
 "caption": "Task list",
 "imgref": "images/Task-list-12.png"
 },{
-"caption": "pension details",
-"imgref": "images/pension-details.png"
+"caption": "Pension provider",
+"imgref": "images/pension-provider.png"
+},{
+"caption": "Pension type",
+"imgref": "images/pension-type.png"
+,{
+"caption": "Pension amount",
+"imgref": "images/pension-amount.png"
 },{
 "caption": "Check your answers",
 "imgref": "images/"
